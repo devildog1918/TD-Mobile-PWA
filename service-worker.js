19b06818b7054f31e4ceb2d5ba57fe19fc9e54a4
devildog1918 <robert.perryman27@gmail.com>
2026-04-17 05:42:39 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,16 @@
-const CACHE_NAME = 'rp-ticket-assistant-v1';
-const APP_SHELL = [
+const CACHE_NAME = 'rp-blurb-library-v1';
+const APP_ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './service-worker.js',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))
   );
   self.skipWaiting();
 });
@@ -30,9 +31,6 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
 
-  const requestUrl = new URL(event.request.url);
-  if (requestUrl.origin !== self.location.origin) return;
-
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
       if (cachedResponse) {
@@ -41,16 +39,10 @@ self.addEventListener('fetch', (event) => {
 
       return fetch(event.request)
         .then((networkResponse) => {
-          if (
-            networkResponse &&
-            networkResponse.status === 200 &&
-            networkResponse.type === 'basic'
-          ) {
-            const responseClone = networkResponse.clone();
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, responseClone);
-            });
-          }
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
           return networkResponse;
         })
         .catch(() => caches.match('./index.html'));
